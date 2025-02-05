cea733fa8f204e5d84a8a334c698370c4c7aa950
Sandro Granados <dimesandro@gmail.com>
2025-02-05 11:18:39 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -86,9 +86,6 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
-def setup(app):
-   app.add_stylesheet('css/custom.css')
 
 from recommonmark.parser import CommonMarkParser
 
